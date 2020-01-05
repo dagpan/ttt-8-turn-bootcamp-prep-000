62d61181deadccb260c1e66b283cb8d2d94ec53f
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-05 23:34:00 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,87 @@
+# TOKEN_BOARD  METHOD
+
+#def token_board(token)
+#    token_value = ["X","O"]
+#    value = token_value[token]
+#end
+
+# DISPLAY_BOARD  METHOD
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# INPUT_TO_INDEX  METHOD
+
+def input_to_index(user_input)
+  input = user_input.to_i
+  index = input - 1
+  return index
+end
+
+# MOVE  METHOD
+
+def move(board, index, value)
+  board[index] = value
+end
+
+# VALID_MOVE?  METHOD
+
+def valid_move?(board, index)
+  if index > 0 && index < 9 
+     if position_taken?(board, index) == false
+        return true
+     else
+        return false
+     end
+  else
+     return false
+  end
+end
+
+# POSITION_TAKEN?  METHOD
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+     return true
+  else
+     if board[index] == " " || board[index] == "" || board[index] == nil
+        return false
+     end
+  end
+end
+
+# TURN  METHOD
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+#  input = user_input.to_i
+#  index = input - 1
+  input_to_index(user_input)
+  if valid_move?(board, index) == true
+#     turn_count = turn_count + 1
+     if turn_count.odd?
+#        value = "X"
+#     else
+#        value = "O"
+#     end
+     move(board, index, value = "X")
+     display_board(board)
+     else
+       puts "The move you selected is invalid, pleae try again!"
+#     valid_turn = 2
+       turn(board)
+     end
+#  end
+end
+
+#valid_turn = 0
+#while valid_turn < 10
+#      turn(board)
+#      valid_turn += 1
+#end
